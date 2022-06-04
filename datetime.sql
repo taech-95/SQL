78277409dbdf543c2fd782a95e6e9d76472312f6
Максимов Микола <mykola.maksymov@gmail.com>
2022-06-04 08:57:44 +0000

    data types

---
+++ datetime.sql
@@ -0,0 +1,9 @@
+create table people (
+    name varchar(100),
+    birthDate Date,
+    birthTime Time,
+    birthghd DATETIME
+);
+
+insert into people (name, birthDate, birthTime, birthghd) values ('Mykola', '1995-01-08', '08:55:27', '1995-01-08 08:55:27');
+insert into people (name, birthDate, birthTime, birthghd) values ('Oksana', '1975-01-08', '12:55:27', '1975-01-08 12:55:27');
